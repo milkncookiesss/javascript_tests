5ff06fa6607de8c32d6c2855a76e45399967e0d1
milkncookiesss <p.parkcontact@gmail.com>
2019-04-27 15:32:12 -0700

    "basic test working, requires module to export"

--- spec/tests.js
+++ spec/tests.js
@@ -4,32 +4,39 @@ const should = require('chai').should;
 const expect = require('chai').expect;
 //have files required from src here
 
-
+// define(['../src/jsreview.js'])
+const sum = require('../src/jsreview.js')
 //have before hooks (if needed) here
 
 
-
-
-
-//array practice
-describe('Array', () => {
-  describe('Fun with Arrays', () => {
-    it('should exist', () => {
-      var arr = {};
-      assert.isArray(arr, 'is array');
-    })
-    it('should return -1 when the value is no present', () => {
-      assert.equal([1,2,3].indexOf(4), -1);
-    })
+describe('sum', () => {
+  // var sum;
+  console.log('what are you ', sum);
+  it('should exist', () => {
+    expect(typeof sum).to.equal('function');
   })
 })
 
-//object practice
-describe('Objects', () => {
 
-});
+// //array practice
+// describe('Array', () => {
+//   describe('Fun with Arrays', () => {
+//     it('should exist', () => {
+//       var arr = {};
+//       assert.isArray(arr, 'is array');
+//     })
+//     it('should return -1 when the value is no present', () => {
+//       assert.equal([1,2,3].indexOf(4), -1);
+//     })
+//   })
+// })
 
-//function practice
-describe('Functions', () => {
+// //object practice
+// describe('Objects', () => {
 
-});
+// });
+
+// //function practice
+// describe('Functions', () => {
+
+// });
--- src/jsreview.js
+++ src/jsreview.js
@@ -1,5 +1,5 @@
 function sum() {
-  var args = arguments.slice();
+  var args = Array.prototype.slice.call(arguments, 1);
   var sum = 0;
   args.forEach((num) => {
     sum += num;
@@ -7,4 +7,4 @@ function sum() {
   return sum;
 }
 
-module.exports = {sum};
+module.exports = sum;
