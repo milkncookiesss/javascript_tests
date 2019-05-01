040f55ec0428ff510985d198f01512fdbdfbb3fd
milkncookiesss <p.parkcontact@gmail.com>
2019-04-30 21:41:50 -0700

    "adding more files and trying to put on html"

--- spec/tests.js
+++ spec/tests.js
@@ -1,26 +1,75 @@
-// const assert = require('assert');
 const assert = require('chai').assert;
 const should = require('chai').should;
 const expect = require('chai').expect;
 //have files required from src here
 
 // define(['../src/jsreview.js'])
-const array = require('../src/arrays.js')
+const array = require('../src/arrays.js');
+const obj = require('../src/objects.js');
+const func = require('../src/functions.js');
 //have before hooks (if needed) here
 
 describe('Array', () => {
+    
   it('should be an array', () => {
     expect(Array.isArray(array.arr)).to.equal(true);
   })
-  it('should have length of 5', () => {
-    expect(array.arr.length).to.equal(5);
+
+  it('should have length of 6', () => {
+    expect(array.arr.length).to.equal(6);
   })
-  it('should exist', () => {
-    expect(typeof array.findOddNums).to.equal('function');
+
+  it('should contain a string', () => {
+    var string = array.arr[array.arr.length - 1];
+    expect(typeof string).to.equal('string');
   })
-  it('should return array of odd numbers', () => {
-    var newArr = [1,[2],{a: 15},4,"5",6,7,]
-    expect(array.findOddNums(newArr)).to.deep.equal([1,7])
+
+  describe('Array First Function', () => {
+
+    it('should exist', () => {
+      expect(typeof array.first).to.equal('function');
+    })
+
+    it('should return the first element of an array', () => {
+      var newArr = [];
+      newArr.push(1);
+      newArr.push('hello');
+      expect(array.first(newArr)).to.equal(1);
+    })
+  })
+
+  describe('Array Last Function', () => {
+
+    it('should exist', () => {
+      expect(typeof array.last).to.equal('function');
+    })
+
+    it('should return the last element of an array', () => {
+      var newArr = [];
+      newArr.push('hello');
+      newArr.push('world');
+      newArr.push(15);
+      expect(array.last(newArr)).to.equal(15);
+    })
+  })
+
+  describe('Array function practice', () => {
+
+    it('should return array of odd numbers', () => {
+      var newArr = [1,[2],{a: 15},4,"5",6,7,]
+      expect(array.findOddNums(newArr)).to.deep.equal([1,7])
+    })
+
+    it('should return the sum of an array', () => {
+      var newArr = [1,2,3,4,5,6];
+      expect(array.findSum(newArr)).to.equal(21);
+    })
+
+    it('should return the sum of the array that contains nested arrays', () => {
+      var newArr = [1,2,3,[4,5,6],7,8,9];
+      expect(array.findSum(newArr)).to.equal(45);
+    })
   })
 })
 
+mocha.run();
--- src/arrays.js
+++ src/arrays.js
@@ -1,16 +1,55 @@
 //Write code here, Do NOT change variable names.
-
-//create an array
 var arr;
+//create an array named arr with 5 elements
+
+//add a string into your array
+
+//create a function called "first" that returns the first element of an array
+
+//create a function called "last" that returns the last element of an array
+var last;
+
 
 //fill in this function that accepts a parameter that is an array and returns a new array of all the odd numbers from the parameter
 function findOddNums() {};
 
 //fill in this function that takes in an array and returns the sum of all the elements
-function findSum() {};
+function findSum(x) {
+  var sum = 0;
+  for (var i = 0; i < x.length; i++) {
+    sum += x[i];
+  }
+  return sum
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 module.exports = {
   arr: arr,
+  first: first,
+  last: last,
   findOddNums: findOddNums,
   findSum: findSum
   };
