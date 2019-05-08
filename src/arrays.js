51bd781169ff53ace920e8ac1b81306452c1beb5
milkncookiesss <p.parkcontact@gmail.com>
2019-05-07 18:29:46 -0700

    "writing read me"

--- src/arrays.js
+++ src/arrays.js
@@ -3,17 +3,16 @@
 
 
 //create an array named arr
-var arr = [];
+
 //add 5 elements to the array using the push method with the last element being a string
 
 //create a function called "first" that returns the first element of an array
-var first = function() {};
+
 //create a function called "last" that returns the last element of an array
-var last = function() {};
+
 //create a function called findOddNums that accepts a parameter that is an array and returns a new array of all the odd numbers from the given array
-var findOddNums = function() {};
+
 //create a function called findSum that takes in an array and returns the sum of all the elements
-var findSum = function() {};
 
 
 
