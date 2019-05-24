9e4be966e72887cc85b8e08fee3e0977a961d7da
milkncookiesss <p.parkcontact@gmail.com>
2019-05-23 17:48:29 -0700

    "finished version 1 of exercise to give to students"

--- spec/functest.js
+++ spec/functest.js
@@ -2,7 +2,7 @@ const assert = require('chai').assert;
 const should = require('chai').should;
 const expect = require('chai').expect;
 
-
+const {sum, mult, replaceLetters} = require('../src/functions.js')
 describe('functional programming', () => {
 
   describe('sum function', () => {
@@ -12,18 +12,30 @@ describe('functional programming', () => {
     })
 
     it('should return sum of two numbers', () => {
-      expect(sum()).to.equal();
+      expect(sum(1, 2)).to.equal(3);
+    })
+
+    it('should return the sum of all the arguments', () => {
+      expect(sum(1, 100, 25, 34, 99, 1.5, 11.25, 420)).to.equal(691.75);
+    })
+
+    it('should return 0 if no arguments are given', () => {
+      expect(sum()).to.equal(0);
     })
   })
 
-  describe('multiply function', () => {
+  describe('mult function', () => {
 
     it('should be a function', () => {
-      expect(typeof multiply).to.equal('function');
+      expect(typeof mult).to.equal('function');
     })
 
-    it('should return product', () => {
-      expect().to.equal();
+    it('should return 1 if no arguments are given', () => {
+      expect(mult()).to.equal(1);
+    })
+
+    it('should return the product of all the arguments', () => {
+      expect(mult(25, 2, 3, 15, 9, 6)).to.equal(121500);
     })
   })
 
@@ -34,12 +46,28 @@ describe('functional programming', () => {
     })
 
     it('should return a string', () => {
-      expect().to.equal('string');
+      let result = replaceLetters();
+      expect(typeof result).to.equal('string');
+    })
+
+    it('should return an empty string if there are no arguments', () => {
+      let result = replaceLetters();
+      expect(result).to.equal('');
     })
 
     it(`should replace all e's with p's`, () => {
       let str = 'etpeqtfeijelnme';
-      expect(replacceLetters(str)).to.equal();
+      expect(replaceLetters(str)).to.equal('ptppqtfpijplnmp');
+    })
+
+    it('should switch e with p and E with P', () => {
+      let str = 'Everyone eats Eggs evEry evEning evEntually';
+      expect(replaceLetters(str)).to.equal('Pvpryonp pats Pggs pvPry pvPning pvPntually');
+    })
+
+    it("should return original string if there are no e's", () => {
+      let str = 'you know what is up';
+      expect(replaceLetters(str)).to.equal(str);
     })
   })
 })
--- src/functions.js
+++ src/functions.js
@@ -6,8 +6,48 @@
 //create a function called "sum" that takes in any ammount of numbers and returns the sum
 
 
-//create a function called "multiply" that takes in any amount of arguments that gets 
+//create a function called "mult" that takes in any ammount of arguments and return the product
 
 
-//create a function called "replaceLetters" that takes in a string and replaces the letter 'e' with 'p'
+//create a function called "replaceLetters" that takes in a string and replaces the letter 'e' and 'E' with 'p' and 'P' respectedly
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = {
+  sum: sum,
+  mult: mult,
+  replaceLetters: replaceLetters
+}
